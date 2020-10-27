c037f8811c2b6886275031d3da203d307b47944d
Alie Stone <helloaliestone@gmail.com>
2020-10-27 17:45:27 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,7 +13,7 @@ end
   class Dog
     attr_accessor :name, :breed, :age
 
-    def initialize(name, breed, age)
+    def initialize(name:, breed:, age:)
       @name = name
       @breed = breed
       @age = age
