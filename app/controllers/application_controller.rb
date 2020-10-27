3225f8648a168b38f0c9f6a62a06a49e9bc1e5ef
Alie Stone <helloaliestone@gmail.com>
2020-10-27 17:43:02 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,14 +10,13 @@ class ApplicationController < Sinatra::Base
   end
 end
 
+  class Dog
+    attr_accessor :name, :breed, :age
 
-class Dog
-  attr_accessor :name, :breed, :age
-
-  def initilize(name, breed, age)
-    @name = name
-    @breed = breed
-    @age = age
-  end
+    def initialize(name, breed, age)
+      @name = name
+      @breed = breed
+      @age = age
+    end
 
 end
